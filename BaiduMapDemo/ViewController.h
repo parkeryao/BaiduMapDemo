6afa476b97b7f308205d98fd4e3d69146622cf13
gary.ylyao <gary.ylyao@gmail.com>
2019-03-21 08:56:15 +0800

    Initial Commit

---
+++ BaiduMapDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BaiduMapDemo
+//
+//  Created by Gary Yao on 2019/3/21.
+//  Copyright © 2019 mobilenow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BaiduMapDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BaiduMapDemo
+//
+//  Created by Gary Yao on 2019/3/21.
+//  Copyright © 2019 mobilenow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
