02d0ce2a713c85b98ec4bbe9a066e9492a69a787
gary.ylyao <gary.ylyao@gmail.com>
2019-03-25 09:48:41 +0800

    Revert "实现定位功能"
    This reverts commit ba9b46269f868fb28912525b7355723ce45e9a21.

--- BaiduMapDemo/AppDelegate.m
+++ BaiduMapDemo/AppDelegate.m
@@ -7,11 +7,8 @@
 //
 
 #import "AppDelegate.h"
-#import <BaiduMapAPI_Base/BMKBaseComponent.h>
 
 @interface AppDelegate ()
-
-@property (nonatomic, strong) BMKMapManager *mapManager;
 
 @end
 
@@ -19,16 +16,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    
-    _mapManager = [[BMKMapManager alloc] init];
-    BOOL ret = [_mapManager start:@"UmmL5UXRZIyP7k2256cSRaVjQ42w1rq2"  generalDelegate:nil];
-    if (!ret) {
-        NSLog(@"manager start failed!");
-    }
-    [BMKMapManager setCoordinateTypeUsedInBaiduMapSDK: BMK_COORDTYPE_COMMON];
-    
-    
-    NSLog(@"didFinishLaunchingWithOptions");
+    // Override point for customization after application launch.
     return YES;
 }
 
@@ -36,32 +24,27 @@
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    NSLog(@"applicationWillResignActive");
 }
 
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    NSLog(@"applicationDidEnterBackground");
 }
 
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
     // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    NSLog(@"applicationWillEnterForeground");
 }
 
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    NSLog(@"applicationDidBecomeActive");
 }
 
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    NSLog(@"applicationWillTerminate");
 }
 
 
